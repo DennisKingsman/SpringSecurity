fccdce7c631e54b09e024f7b7e2f6cb4e8bc3e6d
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-08-14 05:43:31 +0400

    preparations

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Author
 ## DennisKingsman 
+# Process
+Step 1  
+Creating a first-sample project  
 
---
+++ first-sample/src/main/java/com/spring/security/firstsample/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.spring.security.firstsample.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication();
+    }
+
+}
