8881d3688c967858ae717ceee312f1d803f326c1
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-08-15 18:27:29 +0400

    fix

--- security-LDAP/src/main/java/com/spring/security/ldap/securityLDAP/config/SecurityConfig.java
+++ security-LDAP/src/main/java/com/spring/security/ldap/securityLDAP/config/SecurityConfig.java
@@ -5,11 +5,20 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -21,17 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .url("ldap://localhost:8389/dc=springframework,dc=org")
                 .and()
                 .passwordCompare()
-                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordEncoder(new BCryptPasswordEncoder())
                 .passwordAttribute("userPassword");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .fullyAuthenticated()
-                .and()
-                .formLogin();
     }
 
 }
