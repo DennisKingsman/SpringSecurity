07dc8e6e98a35f74f7fc99d170835335f548b5a1
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-08-16 00:52:27 +0400

    step 6 completed

---
+++ security-jwt/src/main/java/com/spring/security/jwt/securityjwt/authenticate/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.spring.security.jwt.securityjwt.authenticate;
+
+import java.io.Serializable;
+
+public class AuthenticationResponse implements Serializable {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+}
--- security-jwt/src/main/java/com/spring/security/jwt/securityjwt/config/SecurityConfig.java
+++ security-jwt/src/main/java/com/spring/security/jwt/securityjwt/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.spring.security.jwt.securityjwt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -29,6 +31,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                anyRequest().authenticated();
     }
 
 }
