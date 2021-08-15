b54af45e4d0269c38fccdda7ebb0671f98196458
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-08-15 18:28:14 +0400

    reformat code

--- security-LDAP/src/main/java/com/spring/security/ldap/securityLDAP/SecurityLdapApplication.java
+++ security-LDAP/src/main/java/com/spring/security/ldap/securityLDAP/SecurityLdapApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecurityLdapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityLdapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityLdapApplication.class, args);
+    }
 
 }
--- security-LDAP/src/main/java/com/spring/security/ldap/securityLDAP/config/SecurityConfig.java
+++ security-LDAP/src/main/java/com/spring/security/ldap/securityLDAP/config/SecurityConfig.java
@@ -3,10 +3,8 @@ package com.spring.security.ldap.securityLDAP.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
